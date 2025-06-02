161d3f34c6e26e5e0b3198402eced9fb20f02f05
Samitha Agrapala <160111426+Agrapala@users.noreply.github.com>
2025-06-02 23:42:49 +0530

    complex abs,variance

---
+++ complex.py
@@ -0,0 +1,6 @@
+import cmath
+z = complex(1, 2)  # Create a complex number 1 + 2j
+r=print(abs(z))
+s=print(cmath.phase(z))  # Print the phase of the complex number
+print(r)
+print(s)  # Print the phase of the complex number
